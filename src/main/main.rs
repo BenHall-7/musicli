4bd82889f9917510bb62ee9dd65886f2dfaab254
BenHall-7 <Benjaminjahall@gmail.com>
2021-03-05 08:04:56 -0500

    Enter raw mode to support bash terminal; create AppError enum

--- src/main/main.rs
+++ src/main/main.rs
@@ -7,10 +7,11 @@ use std::{
 use binread::BinRead;
 use crossterm::event::{poll, read, Event, KeyCode};
 use crossterm::execute;
-use crossterm::terminal::SetTitle;
+use crossterm::terminal::{SetTitle, EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode};
 use musiclib::midi::{File, Timing};
 use tui::{backend::CrosstermBackend, Terminal};
 
+mod error;
 mod utils;
 mod widgets;
 
@@ -18,10 +19,13 @@ use widgets::{Piano, PianoState};
 
 const BEETHOVEN: &[u8] = include_bytes!("appass_3.mid");
 
-fn main() {
-    execute!(stdout(), SetTitle("muslici - cli midi editor")).unwrap_or_else(|_| {
-        eprintln!("Unable to change title");
-    });
+fn main() -> Result<(), error::AppError> {
+    execute!(
+        stdout(),
+        SetTitle("muslici - cli midi editor"),
+        EnterAlternateScreen
+    )?;
+    enable_raw_mode()?;
     let mut t = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
     t.clear().unwrap();
 
@@ -68,5 +72,10 @@ fn main() {
         }
     }
 
-    t.clear().unwrap()
+    disable_raw_mode()?;
+    execute!(stdout(), LeaveAlternateScreen)?;
+
+    t.clear().unwrap();
+
+    Ok(())
 }
